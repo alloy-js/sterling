f26b5ddefd23661bc3b9a0180c1e5140017a5328
Tristan Dyer <atdyer@ncsu.edu>
2020-01-30 14:16:17 -0500

    Generate 1.0 preview build

---
+++ src/main/resources/public/precache-manifest.10a3cc66f65a6208d3cd8069e2dc5506.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a31b6b624e79bc04c13392a58a1dd271",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c54b3475dde9088f4160",
+    "url": "/static/css/2.5769cc56.chunk.css"
+  },
+  {
+    "revision": "3d6f12b8f030aa1c4e50",
+    "url": "/static/css/main.4db12827.chunk.css"
+  },
+  {
+    "revision": "c54b3475dde9088f4160",
+    "url": "/static/js/2.3ffa7cc5.chunk.js"
+  },
+  {
+    "revision": "3d6f12b8f030aa1c4e50",
+    "url": "/static/js/main.1f6ce53e.chunk.js"
+  },
+  {
+    "revision": "4a4221b002e538ef86a5",
+    "url": "/static/js/runtime-main.62c01fbb.js"
+  },
+  {
+    "revision": "03ab771b196ad90f28a35a5aa8f5ec08",
+    "url": "/static/media/icons-16.03ab771b.ttf"
+  },
+  {
+    "revision": "84c180eabdd692efd197345a33735e8c",
+    "url": "/static/media/icons-16.84c180ea.woff"
+  },
+  {
+    "revision": "9631870bf93f28bc1d7010bf55621ae8",
+    "url": "/static/media/icons-16.9631870b.eot"
+  },
+  {
+    "revision": "053082015844707d8048e606b922cdad",
+    "url": "/static/media/icons-20.05308201.ttf"
+  },
+  {
+    "revision": "5c61819764f1fa973dda8ef77e391451",
+    "url": "/static/media/icons-20.5c618197.woff"
+  },
+  {
+    "revision": "ca7934b30a0b9829dbeb0b58a6fd0b51",
+    "url": "/static/media/icons-20.ca7934b3.eot"
+  }
+]);
