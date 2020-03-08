c6644b353f1fea4b4e4113460ac110b8aaa8ee33
Tristan Dyer <atdyer@ncsu.edu>
2020-03-08 19:57:14 -0400

    1.0 build

--- src/main/resources/public/service-worker.js
+++ src/main/resources/public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a3fc57854fc228a11728f23364ea9341.js"
+  "./precache-manifest.afd1349d4be86768b14cd8e71ec202aa.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
