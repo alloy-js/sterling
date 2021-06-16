e685cf496136c4b2ed97b3bd2908a8ed9c733878
Tristan Dyer <atdyer@ncsu.edu>
2021-06-16 16:49:51 -0400

    v1.1.0

--- src/main/resources/public/service-worker.js
+++ src/main/resources/public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.9994cfa2f57080a04c145d592c6c7b6f.js"
+  "./precache-manifest.2abbd74fa4a68fc918c77cea202030fc.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
